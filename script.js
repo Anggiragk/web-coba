93a1e8b14313f9de407dd915a75a37061f7c3301
Anggiragk <anggira.gk@gmail.com>
2021-07-31 14:44:52 +0700

    commit awal

---
+++ script.js
@@ -0,0 +1,74 @@
+import "./image-figure.js";
+// const cekPrima = () =>{
+//   let pembagi = 0;
+//   let angka = prompt(`masukkan anggka`);
+
+//   for(let i = 1; i <= angka; i++) {
+//     if(angka%i == 0){
+//       pembagi++;
+//     }
+//   }
+
+//   if(pembagi == 2){
+//     alert(`bilangan prima`);
+//   } else {
+//     alert(`bukan bilangan prima`);
+//   }
+
+// }
+
+// const bilExponsial = (bil) => {
+//   let hasil = '';
+//   if (bil%1== 0) {
+//     hasil = true;
+//   } else {
+//     hasil = false;
+//   }
+//   return hasil;
+// }
+
+// console.log(bilExponsial(25));
+
+// // function kotak(panjang) {
+// //   let hasil = '';
+// //   for (let i = 0; i < panjang; i++) {
+// //       for (let j = 0; j < panjang; j++) {
+// //           hasil += '*';
+// //       }
+// //       hasil += '\n';
+// //   }
+// //   return hasil;
+// // }
+// // console.log(kotak(3));
+
+// // function sikuKiri(panjang) {
+// //   let hasil = '';
+// //   for(let i =0; i<panjang; i++) {
+// //     for(let j = 0; j<=i; j++){
+// //       hasil +='*';
+// //     }
+// //     hasil+='\n';
+// //   }
+// //   return hasil;
+// // }
+
+// // // console.log(sikuKiri(5));
+
+// // for (let i = 0; i <= 5; i++) {
+// //   console.log(`halo ${i} kali`);
+// // }
+
+
+// // class adaw {
+// //   constructor (makan, minum) {
+// //     this.makan = 'nasi';
+// //     this.minum = 'air';
+// //   }
+
+// //   makan() {
+// //     console.log(`${this.makan}`);
+// //   }
+// // }
+
+// // const aku = new adaw(); 
+
